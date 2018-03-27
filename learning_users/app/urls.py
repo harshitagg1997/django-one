c84b005209c37967e4edb616eba97587001fe30f
harshit <harshitaggarwal1997.ha@gmail.com>
2018-03-27 16:43:11 +0530

    first commit

---
+++ learning_users/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from app import views
+
+# SET THE NAMESPACE!
+app_name = 'app'
+
+urlpatterns=[
+
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+
+]
